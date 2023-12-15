b7ea1c3788170241b54c6df7e4426e9b66e0ccf8
Sakuta1126 <maildosteam@gmail.com>
2023-12-15 10:20:18 +0100

    uzupelniono klase gracz

--- MJPMGit/Widoki/Klasy/Gracz.cs
+++ MJPMGit/Widoki/Klasy/Gracz.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MJPMGit.Widoki.Klasy
 {
-    internal class Gracz
+    public class Gracz
     {
+        [AutoIncrement,PrimaryKey]
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Email {  get; set; }
+        public string Kod {  get; set; }
+        public Gracz( int id, string imie, string nazwisko, string email, string kod)
+        {
+            Id = id;
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Email = email;
+            Kod = kod;
+        }
+        public Gracz( string imie, string nazwisko, string email, string kod)
+        {
+            
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Email = email;
+            Kod = kod;
+        }
+        public Gracz()
+        {
+
+           
+        }
     }
 }
