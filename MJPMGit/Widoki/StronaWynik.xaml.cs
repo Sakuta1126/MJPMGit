9d37145e43b1a1e5fa12d9d57d167ba2505c6ef7
p-majchrzak <pjoter606@gmail.com>
2023-12-15 10:47:55 +0100

    uzupelniono strona admin, strona wynik

--- MJPMGit/Widoki/StronaWynik.xaml.cs
+++ MJPMGit/Widoki/StronaWynik.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MJPMGit.Widoki.Klasy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,14 @@ namespace MJPMGit.Widoki
         public StronaWynik()
         {
             InitializeComponent();
+            listaWygranych.ItemsSource = App.Baza.Wypisz<Wynik>();
+        }
+
+        private void wyszukaj_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            List<Wynik> listaWynikow = App.Baza.Wypisz<Wynik>();
+            List<Wynik> nowaLista = listaWynikow.Where(wygrana => wygrana.Id.ToString().Contains(wyszukaj.Text)).ToList();
+            listaWygranych.ItemsSource = nowaLista;
         }
     }
 }
