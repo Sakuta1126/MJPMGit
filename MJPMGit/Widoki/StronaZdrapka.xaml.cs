202da44715648b1ca9745ab507030d6b81ca98cb
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-05 19:23:52 +0100

    Dodano mechanizm przegranej dla gry zdrapki.

--- MJPMGit/Widoki/StronaZdrapka.xaml.cs
+++ MJPMGit/Widoki/StronaZdrapka.xaml.cs
@@ -17,6 +17,7 @@ namespace MJPMGit.Widoki
         bool odkrytoWygrywajacaLiczbe1 = false;
         bool odkrytoWygrywajacaLiczbe2 = false;
         bool odkrytoWygrywajacaLiczbe3 = false;
+        int licznik = 0;
         public StronaZdrapka()
         {
             a = losowa.Next(51);
@@ -43,103 +44,184 @@ namespace MJPMGit.Widoki
             }
         }
 
+        private void BrakWygranych()
+        {
+            licznik++;
+            if(licznik == 9)
+            {
+                DisplayAlert("Przegrana", "Nie udało ci się wygrać.", "ok");
+                _ = Navigation.PushAsync(new StronaGlowna());
+            }
+        }
         private async void OdkryjPole1(object sender, EventArgs e)
         {
             pole1.Text = d.ToString();
-            pole1.ImageSource = "";
-            if (a == d || b == d || c == d)
-            { 
-                await DisplayAlert("Wygrales", "udalo ci sie wygrac glowna nagrode zdrapkowa", "ok");
-                _= Navigation.PushAsync(new StronaGlowna());
+            if (pole1.ImageSource != null)
+            {
+                if (a == d || b == d || c == d)
+                {
+                    pole1.ImageSource = null;
+                    await DisplayAlert("Wygrana", "Gratulację, wygrałeś główną nagrodę.", "ok");
+                    _ = Navigation.PushAsync(new StronaGlowna());
+                }
+                else
+                {
+                    BrakWygranych();
+                }
             }
+            pole1.ImageSource = null;
         }
 
         private async void OdkryjPole2(object sender, EventArgs e)
         {
             pole2.Text = x.ToString();
-            pole2.ImageSource = "";
-            if (a == x || b == x || c == x)
+            if (pole2.ImageSource != null)
             {
-                await DisplayAlert("Wygrales", "udalo ci sie wygrac glowna nagrode zdrapkowa", "ok");
-                _ = Navigation.PushAsync(new StronaGlowna());
+                if (a == x || b == x || c == x)
+                {
+                    pole2.ImageSource = null;
+                    await DisplayAlert("Wygrana", "Gratulację, wygrałeś główną nagrodę.", "ok");
+                    _ = Navigation.PushAsync(new StronaGlowna());
+                }
+                else
+                {
+                    BrakWygranych();
+                }
             }
+            pole2.ImageSource = null;
         }
 
         private async void OdkryjPole3(object sender, EventArgs e)
         {
             pole3.Text = f.ToString();
-            pole3.ImageSource = "";
-            if (a == f || b == f || c == f)
+            if (pole3.ImageSource != null)
             {
-                await DisplayAlert("Wygrales", "udalo ci sie wygrac glowna nagrode zdrapkowa", "ok");
-                _ = Navigation.PushAsync(new StronaGlowna());
+                if (a == f || b == f || c == f)
+                {
+                    pole3.ImageSource = null;
+                    await DisplayAlert("Wygrana", "Gratulację, wygrałeś główną nagrodę.", "ok");
+                    _ = Navigation.PushAsync(new StronaGlowna());
+                }
+                else
+                {
+                    BrakWygranych();
+                }
             }
+            pole3.ImageSource = null;
         }
 
         private async void OdkryjPole4(object sender, EventArgs e)
         {
             pole4.Text = g.ToString();
-            pole4.ImageSource = "";
-            if (a == g || b == g || c == g)
+            if (pole4.ImageSource != null)
             {
-                await DisplayAlert("Wygrales", "udalo ci sie wygrac glowna nagrode zdrapkowa", "ok");
-                _ = Navigation.PushAsync(new StronaGlowna());
+                if (a == g || b == g || c == g)
+                {
+                    pole4.ImageSource = null;
+                    await DisplayAlert("Wygrana", "Gratulację, wygrałeś główną nagrodę.", "ok");
+                    _ = Navigation.PushAsync(new StronaGlowna());
+                }
+                else
+                {
+                    BrakWygranych();
+                }
             }
+            pole4.ImageSource = null;
         }
 
         private async void OdkryjPole5(object sender, EventArgs e)
         {
             pole5.Text = h.ToString();
-            pole5.ImageSource = "";
-            if (a == h || b == h || c == h)
+            if (pole5.ImageSource != null)
             {
-                await DisplayAlert("Wygrales", "udalo ci sie wygrac glowna nagrode zdrapkowa", "ok");
-                _ = Navigation.PushAsync(new StronaGlowna());
+                if (a == h || b == h || c == h)
+                {
+                    pole5.ImageSource = null;
+                    await DisplayAlert("Wygrana", "Gratulację, wygrałeś główną nagrodę.", "ok");
+                    _ = Navigation.PushAsync(new StronaGlowna());
+                }
+                else
+                {
+                    BrakWygranych();
+                }
             }
+            pole5.ImageSource = null;
         }
 
         private async void OdkryjPole6(object sender, EventArgs e)
         {
             pole6.Text = i.ToString();
-            pole6.ImageSource = "";
-            if (a == i || b == i || c == i)
+            if (pole6.ImageSource != null)
             {
-                await DisplayAlert("Wygrales", "udalo ci sie wygrac glowna nagrode zdrapkowa", "ok");
-                _ = Navigation.PushAsync(new StronaGlowna());
+                if (a == i || b == i || c == i)
+                {
+                    pole6.ImageSource = null;
+                    await DisplayAlert("Wygrana", "Gratulację, wygrałeś główną nagrodę.", "ok");
+                    _ = Navigation.PushAsync(new StronaGlowna());
+                }
+                else
+                {
+                    BrakWygranych();
+                }
             }
+            pole6.ImageSource = null;
         }
 
         private async void OdkryjPole7(object sender, EventArgs e)
         {
             pole7.Text=j.ToString();
-            pole7.ImageSource = "";
-            if (a == j || b == j || c == j)
+            if (pole7.ImageSource != null)
             {
-                await DisplayAlert("Wygrales", "udalo ci sie wygrac glowna nagrode zdrapkowa", "ok");
-                _ = Navigation.PushAsync(new StronaGlowna());
+                if (a == j || b == j || c == j)
+                {
+                    pole7.ImageSource = null;
+                    await DisplayAlert("Wygrana", "Gratulację, wygrałeś główną nagrodę.", "ok");
+                    _ = Navigation.PushAsync(new StronaGlowna());
+                }
+                else
+                {
+                    BrakWygranych();
+                }
             }
+            pole7.ImageSource = null;
         }
 
         private async void OdkryjPole8(object sender, EventArgs e)
         {
             pole8.Text=k.ToString();
-            pole8.ImageSource = "";
-            if (a == k || b == k || c == k)
+            if (pole8.ImageSource != null)
             {
-                await DisplayAlert("Wygrales", "udalo ci sie wygrac glowna nagrode zdrapkowa", "ok");
-                _ = Navigation.PushAsync(new StronaGlowna());
+                if (a == k || b == k || c == k)
+                {
+                    pole8.ImageSource = null;
+                    await DisplayAlert("Wygrana", "Gratulację, wygrałeś główną nagrodę.", "ok");
+                    _ = Navigation.PushAsync(new StronaGlowna());
+                }
+                else
+                {
+                    BrakWygranych();
+                }
             }
+            pole8.ImageSource = null;
         }
 
         private async void OdkryjPole9(object sender, EventArgs e)
         {
             pole9.Text = l.ToString();
-            pole9.ImageSource = "";
-            if (a == l || b == l || c == l)
+            if (pole9.ImageSource != null)
             {
-                await DisplayAlert("Wygrales", "udalo ci sie wygrac glowna nagrode zdrapkowa", "ok");
-                _ = Navigation.PushAsync(new StronaGlowna());
+                if (a == l || b == l || c == l)
+                {
+                    pole9.ImageSource = null;
+                    await DisplayAlert("Wygrana", "Gratulację, wygrałeś główną nagrodę.", "ok");
+                    _ = Navigation.PushAsync(new StronaGlowna());
+                }
+                else
+                {
+                    BrakWygranych();
+                }
             }
+            pole9.ImageSource = null;
         }
      
         private void OdkryjWygranaLiczbe1(object sender, EventArgs e)
